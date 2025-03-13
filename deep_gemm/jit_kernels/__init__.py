b3e0b31cdc77da878f02f7fecb83cfc9f0a212c8
Jiaxing Qi <qijiaxing@gmail.com>
2025-03-13 07:53:22 +0000

    add fp8_gemm_1d1d.cuh

--- deep_gemm/jit_kernels/__init__.py
+++ deep_gemm/jit_kernels/__init__.py
@@ -1,4 +1,5 @@
 from .gemm import gemm_fp8_fp8_bf16_nt
+from .gemm_1d1d import gemm_fp8_fp8_bf16_nt_1d1d
 from .m_grouped_gemm import (
     m_grouped_gemm_fp8_fp8_bf16_nt_contiguous,
     m_grouped_gemm_fp8_fp8_bf16_nt_masked
