907c4aa10a2fb5dc8c00ac92a80f4b21eda58a96
Jiaxing Qi <qijiaxing@gmail.com>
2025-03-24 06:20:23 +0000

    disable block_n choices except 64 or 128

--- deep_gemm/jit_kernels/gemm_1d1d.py
+++ deep_gemm/jit_kernels/gemm_1d1d.py
@@ -73,7 +73,9 @@ def get_best_configs(m: int, n: int, k: int, num_groups: int, num_sms: int,
         block_ms = (64 if m <= 64 else 128, )
     else:
         block_ms = (get_m_alignment_for_contiguous_layout(), )
-    block_ns = tuple(range(16, 129, 8))
+    # block_ns = tuple(range(16, 129, 8))
+    # TODO: some block_n may cause TMA (alignment?) error, use 64 or 128 for now.
+    block_ns = (64, 128)
 
     fix_wave_saturate = lambda x: num_sms if x == 0 else x
     get_num_waves = lambda bm, bn: (ceil_div(ceil_div(m, bm) * ceil_div(n, bn) * num_groups, num_sms) if bm else None)
